02832763c622772cc4a1ae627a833e2a54da8197
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-02-03 22:21:59 -0300

    estructure for geolocation

--- src/screens/Cep/CepScreen.js
+++ src/screens/Cep/CepScreen.js
@@ -19,7 +19,15 @@ class CEPScreen extends Component {
 		super(props)
 		this.state = {
 			borderBottomColor: '#fff', 
-			cep: ''
+			cep: '',
+			// Geolocation
+			userLat: '',
+			userLong: '',
+			companyLat: '',
+			companyLong: '',
+
+			// flags
+			fetch: false,
 		}
 	}
 
@@ -44,26 +52,71 @@ class CEPScreen extends Component {
 		this.setState({ cep: masked })
 	}
 
+	unMask = value => {
+    const regex = /[^a-zA-Z0-9]/g;
+    return (value || '')
+      .toString()
+      .replace(regex, '')
+      .replace('R', '');
+  };
+
+	fetchCep = () => {
+		let cep = this.unMask(this.state.cep)
+		console.log(cep)
+		fetch(`http://viacep.com.br/ws/${cep}/json/`)
+		.then(response => response.json())
+		.then(response => {
+			console.log('response cep', response)
+			this.setState({
+				address: response.logradouro,
+				neighborhood: response.bairro,
+				fetch: true
+			})
+		})
+	}
+	
+
 	render(){
 		return (
 			<View style={styles.container}>
 				<HeaderView title="Consulta CEP" onBack={this.onBack} />
 				<View style={styles.subContainer}>
-						<Text style={styles.cep}> CEP </Text>
-						<Item inlineLabel>
-							<Input
-								value={this.state.cep}
-								style={[ styles.input, { borderBottomColor: this.state.borderBottomColor} ]}
-								onChangeText={this.mask} />
-						</Item>
-					<Button block style={ styles.button }>
-						<Text> Consultar </Text>
-					</Button>
+						{!this.state.fetch ? (
+						<View>
+								<Text style={styles.cep}> CEP </Text>
+								<Item inlineLabel>
+									<Input
+										value={this.state.cep}
+										style={[ styles.input, { borderBottomColor: this.state.borderBottomColor} ]}
+										onChangeText={this.mask} />
+								</Item>
+								<Button block style={ styles.button } onPress={ this.fetchCep }>
+									<Text> Consultar </Text>
+								</Button>
+						</View>
+						) : (
+							<View style={{ flex: 1 }}>
+								<View style={ styles.addressContainer }>
+									<Item rounded>
+										<Input />
+									</Item>
+									<Item rounded>
+										<Input />
+									</Item>
+								</View>
+								<Item rounded>
+									<Input />
+								</Item>
+							</View>
+						)}
+						
 				</View>
 			</View>
 		)
 	}
 }
+
+
 
 const styles = { 
 	loginScreen: {
@@ -110,6 +163,13 @@ const styles = {
 		fontWeight: '600',
 		textAlign: 'center',
 		padding: 10
+	},
+	addressContainer: {
+		flex: 1,
+		width: '100%',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		alignItems: 'center'
 	}
 }
 
