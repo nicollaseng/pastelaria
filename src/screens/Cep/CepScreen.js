544a9132f29d875e2de493dd6288ea00fd722c00
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-02-07 00:37:29 -0300

    cep mockup done

--- src/screens/Cep/CepScreen.js
+++ src/screens/Cep/CepScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { ImageBackground, TouchableOpacity } from 'react-native'
-import { View } from 'react-native'
+import { View, Alert } from 'react-native'
 import {
 	 Content,
 	 Form,
@@ -12,6 +12,9 @@ import {
 import { withNavigation } from 'react-navigation'
 import HeaderView from '../../components/HeaderView'
 import VMasker from 'vanilla-masker'
+import { MAPS_KEY } from 'react-native-dotenv'
+import axios from 'axios';
+import geolibe from 'geolib'
 
 class CEPScreen extends Component {
 
@@ -20,17 +23,25 @@ class CEPScreen extends Component {
 		this.state = {
 			borderBottomColor: '#fff', 
 			cep: '',
-			// Geolocation
-			userLat: '',
-			userLong: '',
-			companyLat: '',
-			companyLong: '',
 
 			// flags
 			fetch: false,
+			loading: false,
+
+			// Geolocations
+			addressLatitude: '',
+			addressLongitude: '',
+			userLatitude: '',
+			userLongitude: '',
+
+			//address
+			addressNumber: '',
+			address: '',
+			city: '',
+			neighborhood: ''
 		}
 	}
-
+	
 	navigate = () => {
 		this.props.navigation.navigate('CEP')
 	}
@@ -62,17 +73,47 @@ class CEPScreen extends Component {
 
 	fetchCep = () => {
 		let cep = this.unMask(this.state.cep)
-		console.log(cep)
 		fetch(`http://viacep.com.br/ws/${cep}/json/`)
 		.then(response => response.json())
 		.then(response => {
-			console.log('response cep', response)
 			this.setState({
 				address: response.logradouro,
 				neighborhood: response.bairro,
+				city: response.localidade,
 				fetch: true
 			})
 		})
+		axios.get(`http://www.mapquestapi.com/geocoding/v1/address?key=${MAPS_KEY}&location=3636+avenida francisco sa+fortaleza,60310001`)
+		.then(response => {
+			let userLatitude = response.data.results[0].locations[0].displayLatLng.lat
+			let userLongitude = response.data.results[0].locations[0].displayLatLng.lng
+			this.setState({ userLongitude, userLatitude })
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	}
+
+	checkAddress = () => {
+		const { addressNumber, city, address, userLatitude, userLongitude, cep } = this.state
+		this.setState({ loading: true })
+		axios.get(`http://www.mapquestapi.com/geocoding/v1/address?key=${MAPS_KEY}&location=${addressNumber}+${address}+${city},${this.unMask(cep)}`)
+			.then(response => {
+				let locationLength = response.data.results[0].locations.length
+				let addressLatitude = response.data.results[0].locations[locationLength-1].latLng.lat
+				let addressLongitude = response.data.results[0].locations[locationLength-1].latLng.lng
+				let distance = geolib.getDistance(
+					{latitude: userLatitude, longitude: userLongitude},
+					{latitude: addressLatitude, longitude: addressLongitude}
+			);
+			if(distance > 10000){
+				Alert.alert('Atenção', 'Infelizmente ainda não cobrimos o endereço informado :(')
+			}
+			})
+			.catch(err => {
+				console.log('error', err)
+				Alert.alert('Atenção', 'Verifique o endereço informado')
+			})
 	}
 	
 
@@ -95,18 +136,60 @@ class CEPScreen extends Component {
 								</Button>
 						</View>
 						) : (
-							<View style={{ flex: 1 }}>
+							<View style={styles.confirmAddress}>
+									<Text style={[styles.cep, { fontSize: 16 }]}> CONFIRME SEU ENDEREÇO </Text>
 								<View style={ styles.addressContainer }>
-									<Item rounded>
-										<Input />
-									</Item>
-									<Item rounded>
-										<Input />
-									</Item>
+									<View style={{ width: '75%'}}>
+										<Item stackedLabel>
+											<Label style={styles.label}>Endereço</Label>
+											<Input
+											 style={styles.inputText}
+											 value={this.state.address}
+											 onChangeText={(address) => false }
+											/>
+										</Item>
+									</View>
+									<View style={{ width: '20%'}}>
+										<Item stackedLabel>
+											<Label style={styles.label}>No.</Label>
+											<Input
+											 style={styles.inputText}
+											 value={this.state.addressNumber}
+											 onChangeText={(addressNumber) => this.setState({ addressNumber }) }
+											/>
+										</Item>
+									</View>
 								</View>
-								<Item rounded>
-									<Input />
-								</Item>
+								<View style={ styles.addressContainer }>
+									<View style={{ width: '60%'}}>
+										<Item stackedLabel>
+											<Label style={styles.label}>Bairro</Label>
+											<Input
+											 style={styles.inputText}
+											 value={this.state.neighborhood}
+											 onChangeText={(neighborhood) => false}
+											/>
+										</Item>
+									</View>
+									<View style={{ width: '35%'}}>
+										<Item stackedLabel>
+											<Label style={styles.label}>Cidade</Label>
+											<Input
+											 style={styles.inputText}
+											 value={this.state.city}
+											 onChangeText={(city) => false}
+											/>
+										</Item>
+									</View>
+								</View>
+								<View style={{ width: '90%' }}>
+									<Button block style={ [styles.button, { marginVertical: 10 }] } onPress={ this.checkAddress }>
+										<Text> Confirmar </Text>
+									</Button>
+									<Button block style={ [styles.button, { backgroundColor: '#ffcc00', marginVertical: 10}] } onPress={ () => this.setState({ fetch: !this.state.fetch, cep: '' }) }>
+										<Text> ENDEREÇO ERRADO? </Text>
+									</Button>
+								</View>
 							</View>
 						)}
 						
@@ -165,11 +248,27 @@ const styles = {
 		padding: 10
 	},
 	addressContainer: {
-		flex: 1,
-		width: '100%',
+		// flex: 0.5,
+		// width: '100%',
 		flexDirection: 'row',
 		justifyContent: 'space-around',
 		alignItems: 'center'
+	},
+	confirmAddress: {
+		flex: 1,
+		justifyContent: 'space-around',
+		alignItems: 'center'
+	},
+	inputText: {
+		fontSize: 14,
+		color: '#000',
+		textAlign: 'right',
+		fontWeight: '400'
+	},
+	label: {
+		fontSize: 16,
+		color: '#e60000',
+		fontWeight: '700'
 	}
 }
 
