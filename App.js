7b0760332b387ddd84ed17019736e8e56f497f27
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-02-28 21:08:52 -0300

    wrapped by store

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
 import EntryPoint from './src/index'
 import AppContainer from './src/routes/index'
+import { Provider } from 'react-redux';
+import { Store } from './src/store';
 
 export default class App extends Component{
   render() {
-    return <AppContainer />
+    return (
+      <Provider store={Store}>
+        <AppContainer />
+      </Provider>
+    )
   }
 }
