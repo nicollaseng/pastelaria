5741453447c98bebbb098dc278318e2175b5ea90
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2019-03-17 17:10:57 -0300

    bug fix

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -82,7 +82,7 @@ const SideBar = props => (
     />
     </Content>
     <View style={{justifyContent: 'flex-end'}}>
-        <Text style={styles.version}>Versão 0.5.13beta</Text>
+        <Text style={styles.version}>Versão 0.5.14beta</Text>
     </View>
   </Container>
 );
